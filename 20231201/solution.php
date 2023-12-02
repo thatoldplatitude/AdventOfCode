72536c2513041f81af765f74ea95a4fc15d38a10
thatoldplatitude <itsfirstnamedotlastname@tutanota.com>
2023-12-02 17:59:08 -0500

    Add input data file. & a short test file. process input and just output list of cleaned values

--- 20231201/solution.php
+++ 20231201/solution.php
@@ -1,5 +1,5 @@
 <?php
-
+$dataInput = __DIR__ . '/Data/testData.txt';
 /**
  * Get input of file. Each line has a number, surrounded with useless data.
  * Parse out the first & last digit of the line. Combined that will be the original value that should've been.
@@ -10,9 +10,12 @@
  */
 
 
- // Lets get this file
-$fh = fopen("ourDataInput.txt",'r');
-
+/** 
+ * Lets get this file.
+ * & whatever else we needa ttrack.
+ */
+$fh = fopen($dataInput,'r');
+$values=[];
  // read line by line
 while ($line = fgets($fh)) {
 	/** 
@@ -21,16 +24,23 @@ while ($line = fgets($fh)) {
 	 * then save it in an arr maybe? idk yet. sure.
 	 * */
 	$lineLength = strlen($line);
+	$individualDigits = [];
 	for ($i=0; $i < $lineLength; $i++) { 
 		if(is_numeric($line[$i])){
 			// Save in an array maybe. at least for now.
+			$individualDigits[] = $line[$i];
 		}
 	}
+	$first = $individualDigits[0];
+	$last = $individualDigits[sizeof($individualDigits) -1];
+
+	$lineValue = ($first * 10) + $last;
+	$vals[] = $lineValue;
 	/**
 	 * When we've got the numbers from our line. We'll need
 	 * to grab the 1st and last to then make a 2digit value.
 	 */
 }
-
+print_r($vals);
 //Don't forget to close.
 fclose($fh);
