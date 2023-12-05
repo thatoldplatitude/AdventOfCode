cdb4a8eee5a20a2e70653ef40cd76d2c750d6d94
thatoldplatitude <itsfirstnamedotlastname@tutanota.com>
2023-12-04 21:19:28 -0500

    yayaya ik worst solution and its gonna f me on part 2. i'm just going fast

--- 20231203/solution.php
+++ 20231203/solution.php
@@ -1,6 +1,6 @@
 <?php
 
-$dataInput = __DIR__ . '/Data/testData.txt';
+$dataInput = __DIR__ . '/Data/data.txt';
 
 $fh = fopen($dataInput,'r');
 
@@ -8,9 +8,9 @@ $lineIdx = $cIdx = 0;
 $matrix = [];
 $symbolLocs = [];
 
-while ($char = fgetc($fh)) {	
+while (false !== $char = fgetc($fh)) {	
 	$matrix[$lineIdx][] = $char;
-	echo "${char}";
+	// echo "${char}";
 	if($char == "\n"){
 		$lineIdx++;
 		$cIdx=0;
@@ -21,4 +21,70 @@ while ($char = fgetc($fh)) {
 	$cIdx++;
 }
 
+// print_r($symbolLocs);
+
+foreach ($symbolLocs as $loc) {
+	// print_r($loc);
+	if (is_numeric($matrix[$loc[0]-1][$loc[1]-1])) {
+		$toCheck[] = [$loc[0]-1,$loc[1]-1];
+	}
+	if (is_numeric($matrix[$loc[0]-1][$loc[1]])) {
+		$toCheck[] = [$loc[0]-1,$loc[1]];
+	}
+	if (is_numeric($matrix[$loc[0]-1][$loc[1]+1])) {
+		$toCheck[] = [$loc[0]-1,$loc[1]+1];
+	}
+	if (is_numeric($matrix[$loc[0]][$loc[1]-1])) {
+		$toCheck[] = [$loc[0],$loc[1]-1];
+	}
+	if (is_numeric($matrix[$loc[0]][$loc[1]+1])) {
+		$toCheck[] = [$loc[0],$loc[1]+1];
+	}
+	if (is_numeric($matrix[$loc[0]+1][$loc[1]-1])) {
+		$toCheck[] = [$loc[0]+1,$loc[1]-1];
+	}
+	if (is_numeric($matrix[$loc[0]+1][$loc[1]])) {
+		$toCheck[] = [$loc[0]+1,$loc[1]];
+	}
+	if (is_numeric($matrix[$loc[0]+1][$loc[1]+1])) {
+		$toCheck[] = [$loc[0]+1,$loc[1]+1];
+	}
+
+	// print_r($toCheck);
+}
+print_r($toCheck);
 fclose($fh);
+
+
+foreach ($toCheck as $value) {
+	$row = $value[0];
+	$col = $value[1];
+	$place = $matrix[$row][$col];
+
+	$valz[$row]=[];
+
+	// echo "START: ROW: ${row}. COL: ${col}\n";
+	while(is_numeric($place)){
+		// array_push($valz[$row],$place);
+		$valz[$row][$col] = $place;
+		$place = $matrix[$row][++$col];
+	}
+
+	$col = $value[1]-1;
+	$place = $matrix[$row][$col];
+	while(is_numeric($place)){
+		array_unshift($valz[$row],$place);
+		// $valz[$row][$col] = $place;
+		$place = $matrix[$row][--$col];
+	}
+	$fin[$row][$col+1] = implode($valz[$row]);
+}
+print_r($fin);
+
+$sum = 0;
+foreach ($fin as $f) {
+	foreach ($f as $fVal) {
+		$sum += $fVal;
+	}
+}
+echo "Sum: ${sum}\n";
